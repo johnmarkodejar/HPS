f55d4cf14f2982561f7b390a0699e1510b370b2a
John Mark Reyes <johnmarkodejar@gmail.com>
2015-03-10 16:50:06 +0800

    2nd commit
    completed the game.

--- client/hps.js
+++ client/hps.js
@@ -1,5 +1,6 @@
  var c = 10;
  var int = false;
+ var selected = false;
 if (Meteor.isClient) {
    Template.gameroom.helpers({
 	 players: function() {
@@ -25,48 +26,122 @@ if (Meteor.isClient) {
 				var player = res['player'];
 				Game.update({_id:id},{"player":player,"occupied":"yes"});	
 				Session.set("player",player);
+				$(".join").hide();
 			}
 		}
-	  });
+	});
+	Template.counter.ctr = function () {
+      return Session.get("dateval");
+    };
+	
+	var setInt = setInterval(function () {
+		var start = Timer.find({start:1}).count();
+		if(start != 0){
+			if(c==0){
+				c = 10;	
+				var res = Timer.findOne();
+				var id = res['_id'];
+				selected = false;
+				var arr = new Array();
+				setTimeout(function(){
+					var ans = "";
+					Answer.find().fetch().forEach(function(obj){
+						var answer = obj['answer'];									 
+						var ap = obj['player'];
+						ans += "<div>Player "+ap+" selected: "+answer+"</div>";
+						arr[ap] = answer;
+						Timer.update({_id:id},{"start":0});	
+						$(".start").show();
+						$("#selectOpt").hide();	
+					});
+					var winner;
+					switch(arr[1]){
+						case "paper":
+							if(arr[2]=="rock"){
+								winner = 1;
+							}else if(arr[2]=="paper"){
+								winner = 0;
+							}else if(arr[2]=="scissor"){
+								winner = 2;	
+							}
+						break;
+						case "rock":
+							if(arr[2]=="rock"){
+								winner = 0;
+							}else if(arr[2]=="paper"){
+								winner = 2;
+							}else if(arr[2]=="scissor"){
+								winner = 1;	
+							}
+						break;
+						case "scissor":
+							if(arr[2]=="rock"){
+								winner = 2;
+							}else if(arr[2]=="paper"){
+								winner = 1;
+							}else if(arr[2]=="scissor"){
+								winner = 0;	
+							}
+						break;
+					}
+					if(winner == 0){
+						winner = "It's a tie.";	
+					}else if(winner == undefined){
+						winner = "No contest. Both player should select bet.";	
+					}else{
+						winner = "Winner is player "+winner;	
+					}
+					ans += "<h3>"+winner+"</h3>";
+					$("#answer").show().html(ans);
+				},3000);
+			}else{
+				if(!selected){
+					$("#selectOpt").show();	
+				}
+				$(".start").hide();
+				c-=1;
+			}	
+			Session.set("dateval",c);
+		}else{
+			Session.set("dateval","");
+		}
+		
+	 }, 1000);
+	
 	Template.game.events({
 		'click .start': function startgame(){
 			  var cnt = Answer.find().count();
-			  if(cnt==0){
-				Answer.insert({"player":"1","answer":""});  
-				Answer.insert({"player":"2","answer":""});  
-			  }
-			  var player = Session.get("player");
-			  var res = Answer.findOne({player:player});
-			  console.log(res['player']);
-			 int = setInterval(function () {
-				if(c==0){
-					c = 10;	
-					clearInterval(int);
-					int = false;
-				}else{
-					c-=1;	
-				}	
-				
-				$(".counter").html(c);
-			  }, 1000);	  
-			 
+			  
+			  //reset the answer of player 1 and 2
+			    Answer.find().fetch().forEach(function(obj){
+					var aid = obj['_id'];									 
+					var ap = obj['player'];
+					Answer.update({"_id":aid},{"player":ap,"answer":""});
+				});
+			  
+			  
+			  //restart the timer
+			  var restart = Timer.findOne();
+			  var id = restart['_id'];
+			  c = 10;
+			  Timer.update({_id:id},{"start":1});
 		  }
 	  });
   Template.options.events({
     'click input': function(e){
+		selected = true;
+		$("#selectOpt").hide();
 		var val = $(e.target).val();
-		if(int){
-			Session.set("player1",val);	
-		}else{
-			alert("Selection not yet allowed");
-			return false;
-		}
+		$("#answer").show().html("You selected "+val);
+		var player = Session.get("player");
+		var res = Answer.findOne({player:player}); 
+		var id = res['_id'];
+		Answer.update({"_id":id},{"player":player,"answer":val});
 	}
   });
-  
+  /*
   
   function startgame(){
-	  alert("test");
 	  var cnt = Answer.find().count();
 	  console.log(cnt);
 	  if(cnt==0){
@@ -89,4 +164,5 @@ if (Meteor.isClient) {
 	  }, 1000);	  
 	 
   }
+  */
 }
--- client/template/profile.js
+++ client/template/profile.js
@@ -9,8 +9,26 @@ Template.profile.helpers({
 });
 Template.profile.events({
 	"click .reset":function(){
-		Game.remove();
+		Game.find().fetch().forEach(function(obj){
+			var id = obj['_id'];									 
+			Game.remove({"_id":id});	
+		});
+		
 		Game.insert({"player":"1","occupied":"no"});
 		Game.insert({"player":"2","occupied":"no"});
+		
+		
+		Answer.find().fetch().forEach(function(obj){
+			var id = obj['_id'];									 
+			Answer.remove({"_id":id});	
+		});
+		Answer.insert({"player":"1","answer":""});
+		Answer.insert({"player":"2","answer":""});
+		
+		Timer.find().fetch().forEach(function(obj){
+			var id = obj['_id'];									 
+			Timer.remove({"_id":id});	
+		});
+		Timer.insert({"start":0});
 	}									  
 });
